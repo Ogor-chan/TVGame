fa7300c9723b0688b024344541680c785493614b
OgorekHD <igor10126@wp.pl>
2024-09-16 19:11:38 +0200

    Zooming in and out

--- TVGame/Assets/Scripts/PlayerControl.cs
+++ TVGame/Assets/Scripts/PlayerControl.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class PlayerControl : MonoBehaviour
 {
-
-
     [SerializeField] private GameObject _pauseMenu;
+    [SerializeField] private GameObject _camera;
+    [SerializeField] private Animator _animCamera;
 
     private bool _pauseLock = false;
+
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && !_pauseLock)
@@ -16,5 +19,21 @@ public class PlayerControl : MonoBehaviour
             _pauseMenu.SetActive(!_pauseMenu.activeInHierarchy);
             _pauseLock = true;
         }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Ending();
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            _animCamera.Play("CameraZoomIn");
+        }
+
+    }
+
+    public void Ending()
+    {
+        _animCamera.Play("CameraZoomOut");
     }
 }
